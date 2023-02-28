81d9266b99b36d1951d1381d588c716a981acbc3
soo <lsy971028@gmail.com>
2023-02-28 11:50:39 +0900

    3nd send

--- jspwork/JspAjaxProject/src/main/java/ajaxboard/model/AjaxBoardDao.java
+++ jspwork/JspAjaxProject/src/main/java/ajaxboard/model/AjaxBoardDao.java
@@ -2,7 +2,9 @@ package ajaxboard.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import mysql.db.DbConnect;
 
@@ -40,4 +42,45 @@ public class AjaxBoardDao {
 	}
 	
 	//select
+	public ArrayList<AjaxBoardDto> getAllAboardDatas()
+	{
+		
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		String sql="select * from ajaxboard order by num desc";
+		
+		ArrayList<AjaxBoardDto> list=new ArrayList();
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			
+			while(rs.next())
+			{
+				AjaxBoardDto dto=new AjaxBoardDto();
+				
+				dto.setNum(rs.getString("num"));
+				dto.setWriter(rs.getString("writer"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setContent(rs.getString("content"));
+				dto.setAvata(rs.getString("avata"));
+				dto.setWriteday(rs.getTimestamp("writeday"));
+				
+				list.add(dto);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, pstmt, conn);
+		}
+		
+		
+		
+		
+		return list;
+	}
+	
 }
